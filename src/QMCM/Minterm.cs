47470c2b28e901fd2484e749fa65ddaaf800be48
Tyler Fanuele <tyler_fanuele@student.uml.edu>
2021-10-01 12:22:06 -0400

    changed minterm
    added constructer and added Value_List member variable

--- src/QMCM/Minterm.cs
+++ src/QMCM/Minterm.cs
@@ -29,13 +29,39 @@ public class Minterm
             }
         }
     }
+    
+    //holds the minterm value history of the minterm
+    List<int> _value_list;
+    public List<int> Value_List
+    {
+        get;
+        set;
+    }
+
     //Updated count of ones and zeroes
     public int Ones, Zeroes;
 
     public Minterm(int val, int variableCount)
     {
         Value = val;
+        Value_List.Add(val);
         Binary = Convert.ToString(val, 2).PadLeft(variableCount, '0');
     }
+
+    //constructor to combine 2 minterms
+    public Minterm(Minterm first, Minterm second)
+    {
+        Value_List.AddRange(first.Value_List);//combine the Value_Lists 
+        Value_List.AddRange(second.Value_List);
+        List<string> temp;
+        for(int i = 0; i < first.Binary.Length; i++)//start of combining the binarys
+        {
+            if(first.Binary[i] == second.Binary[i])
+            {
+                temp.Add(first.Binary[i])
+            }
+        }
+    }
+
 
 }
